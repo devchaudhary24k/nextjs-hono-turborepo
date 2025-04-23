3876c56af721e9a5244bd6e1c2c57dcd122973d4
Dev Talan <chaudhary.dev.talan@gmail.com>
2025-04-23 14:47:22 +0000

    added more features to eslint

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,8 +1,10 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import checkFile from "eslint-plugin-check-file";
+import eslintPluginN from "eslint-plugin-n";
+import onlyWarn from "eslint-plugin-only-warn";
 import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
-import onlyWarn from "eslint-plugin-only-warn";
 
 /**
  * A shared ESLint configuration for the repository.
@@ -16,9 +18,36 @@ export const config = [
   {
     plugins: {
       turbo: turboPlugin,
+      n: eslintPluginN,
+      "check-file": checkFile,
     },
     rules: {
       "turbo/no-undeclared-env-vars": "warn",
+      "prefer-arrow-callback": "error",
+      "prefer-template": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports" },
+      ],
+      "n/no-process-env": "error",
+      "check-file/filename-naming-convention": [
+        "error",
+        {
+          "**/*.{ts,tsx}": "KEBAB_CASE",
+        },
+        {
+          // ignore the middle extensions of the filename to support filename like bable.config.js or smoke.spec.ts
+          ignoreMiddleExtensions: true,
+        },
+      ],
+      "check-file/folder-naming-convention": [
+        "error",
+        {
+          // all folders within src (except __tests__ and Dynamic Routes )should be named in kebab-case
+          "src/**/!(__tests__)": "KEBAB_CASE",
+          "src/**/!^[.*": "KEBAB_CASE",
+        },
+      ],
     },
   },
   {
